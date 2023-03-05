57cb78dfc5ac86cb7f4a21e6580cdd555cadea87
Aditi Dubey <aditidubey576@gmail.com>
2023-03-05 19:04:29 +0530

    completed the question

---
+++ Value, Variables, Types and Operators/09 - Ass level 3.1/code/index.js
@@ -0,0 +1,18 @@
+/*
+🎖 Write a program to calculate the total price of your phone purchase. With these conditions
+ * [ ] You will keep purchasing phones (hint: loop!) until you run out of bank balance.
+ * [ ] You'll also buy accessories for each phone as long as your purchase amount is below your spending threshold.
+ * [ ] After you've calculated your purchase amount, add in the tax, then print out the calculated purchase amount, properly formatted like ($334.76).
+ * [ ] Finally, check the amount against your bank account balance to see if you can afford it or not.
+*/
+
+// ⛑ Answer of the above will `$334.76`.
+
+
+const SPENDING_THRESHOLD = 200;
+const TAX_RATE = 0.08;
+const PHONE_PRICE = 99.99;
+const ACCESSORY_PRICE = 9.99;
+
+var bank_balance = 303.91;
+var amount = 0;
