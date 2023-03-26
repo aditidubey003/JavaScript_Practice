2ad5198e2b0804712277e0c7f5d53d95151c014d
Aditi Dubey <aditidubey576@gmail.com>
2023-03-26 10:46:41 +0530

    completed an assignment

--- Array and String Methods/block-BJaadf/code/index.js
+++ Array and String Methods/block-BJaadf/code/index.js
@@ -81,59 +81,115 @@ console.log(lowercaseFruits);
 let uppercaseFruits =  fruits.map((fruits) => fruits.toUpperCase());
 console.log(uppercaseFruits);
 
-// Convert the numbers array to one level element. Remove sub-arrays to individual elements and log them
+// Other method 
 
+/*
+let uppercaseFruits =[];
+fruits.forEach(function(fruit){
+    uppercaseFruits.push(fruit.toUpperCase());
+});
+*/
+
+
+let numbers = [1, 2, [3, 4]];
+let numbersTwo = [1, 2, [3, 4, [5, 6]]];
+let numbersThree = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+
+// Convert the numbers array to one level element. Remove sub-arrays to individual elements and log them
+console.log(numbers.flat(2));
 
 // Convert the numbersTwo array to one level element. Remove all sub-arrays to individual elements and log them.
 // (for multiple level flat accepts argument by default it's 1 `array.flat(2)`)
 
-
+console.log(numbersTwo.flat(2));
 
 // Convert the numbersThree array to one level element.
 // Remove all sub-arrays to individual elements, log them and update the value of numbersThree to the new flat array.
-
+ console.log(numbersThree.flat(Infinity));
 
 
 // Use forEach to log all the elements of numberThree array
+// Here, we need to reassign the value of numbersThree again.
+numbersThree = numbersThree.flat(Infinity);
 
+numbersThree.forEach((num) => console.log(num));
 
 // Create a new variable named doubleNumbers that store the numberThree array (each element should be multiplied by 2). Use map
-
-
+let doubleNumbers = numbersThree.map(double)
+function double(num){
+    console.log(num * 2,  "resultwith2");
+}
 
 // Create a new variable named tripleNumbers that store the numberThree array (each element should be multiplied by 3). Use map
-
+let tripleNumbers = numbersThree.map(triple)
+function triple(num){
+    console.log(num * 3,  "resultwith3");
+}
 
 // Create a new variable named halfNumbers that store the numberThree array (each element should be divided by 2). Use map
-
+let halfNumbers = numbersThree.map(half)
+function half(num){
+    console.log(num / 2,  "dividedby2");
+}
 
 
 // Create a new variable named oddNumbers that store all the odd numbers in numbersThree array
+/*
+let oddNumbers = numbersThree.map(odd)
+ 
+let oddArray = [];
+function odd(num){
+    if(num % 2 === !0){
+        oddArray.push(num)
+    }
+   //  console.log();
+}*/
 
+// let oddNumbers1 = numbersThree.map((num) => console.log(num % 3 === !0) )
+
+
+// ----------------------
+// By Filter Method , It returns the shorten array.
+
+
+let oddNumbers =  numbersThree.filter(isOdd);
+
+function isOdd(num){
+    return num % 2 !== 0;
+    
+}
+console.log(oddNumbers);
 
 
 // Create a new variable named evenNumbers that store all the even numbers in numbersThree array
+let evenNumbers = numbersThree.filter(
+   (num) => num % 2 === 0
+)
 
-
-
+console.log(evenNumbers);
 
 // Find the index of 10 in numbersThree array
+let index = numbersThree.findIndex((num) => num === 10);
+console.log(index);
 
-
+// Other method 
+/*let index = numbersThree.find((num) => num === 10);
+console.log(index);*/
 
 // Reverse the values of numbersThree array
-
+numbersThree.reverse();
 
 
 // Reverse the values of numbersTwo array
+numbersTwo.reverse();
 
 
 // Join all fruits with '-', convert to uppercase and log it
-
+console.log(fruits.join('_').toUpperCase());
 
 
 // Join all fruits with '&', convert to lowercase and log it
 
-
+console.log(fruits.join('&').toLowerCase());
 
 
