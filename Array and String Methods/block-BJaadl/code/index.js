8308455b272882df14cf60da4d9ccc3960f64ced
Aditi Dubey <aditidubey576@gmail.com>
2023-04-04 21:14:14 +0530

    completed few questions

---
+++ Array and String Methods/block-BJaadl/code/index.js
@@ -0,0 +1,99 @@
+let words = [
+  'mystery',
+  'brother',
+  'aviator',
+  'crocodile',
+  'pearl',
+  'orchard',
+  'crackpot',
+  'rhythm',
+];
+
+// - Write a function findLongestWord that takes an array of words and returns the longest word from the array. (Use above array "words" to test it). If there are 2 with the same length, it should return the first occurrence.
+function findLongestWord(array){
+return [...array]
+.sort((a, b) => a.length - b.length) 
+.pop();
+}
+
+findLongestWord(words);
+
+// - Convert the above array "words" into an array of length of word instead of word.
+let wordsLength = words.map((string) => string.length)
+console.log(wordsLength);
+
+// - Create a new array that only contains word with atleast one vowel.
+let vowelWord =  words.filter((string) => {
+ if(
+  string.toLowerCase().includes('a')  ||
+  string.toLowerCase().includes('e')  ||
+  string.toLowerCase().includes('i')   ||
+  string.toLowerCase().includes('o')   || 
+  string.toLowerCase().includes('u')
+ ) {
+  return true
+ } else {
+  return false
+ }
+} 
+)
+console.log(vowelWord);
+
+// - Find the index of the word "rhythm"
+
+
+
+// - Create a new array that contians words not starting with vowel.
+
+
+// - Create a new array that contianse words not ending with vowel
+
+
+
+let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+// - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its numbers
+
+
+// - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...]
+
+
+// - Create a new array that contains only even numbers
+
+
+// - Create  a new array that contains only odd numbers.
+
+
+// - Create a new array that should have true for even number and false for odd numbers.
+
+
+// - Sort the above number in assending order.
+
+
+
+// - Does sort mutate the original array?
+
+//Yes
+
+// - Find the sum of the numbers in the array.
+
+
+
+//- Write a function averageNumbers that receives an array of numbers and calculate the average of the numbers
+
+
+let strings = [
+  'seat',
+  'correspond',
+  'linen',
+  'motif',
+  'hole',
+  'smell',
+  'smart',
+  'chaos',
+  'fuel',
+  'palace',
+];
+
+// - Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
+
