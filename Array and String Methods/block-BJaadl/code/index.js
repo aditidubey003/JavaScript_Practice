97b49d63493f7c601db98585deef20d289ef4eda
Aditi Dubey <aditidubey576@gmail.com>
2023-04-05 20:05:06 +0530

    completed few qUESTIONS

--- Array and String Methods/block-BJaadl/code/index.js
+++ Array and String Methods/block-BJaadl/code/index.js
@@ -40,29 +40,88 @@ let vowelWord =  words.filter((string) => {
 console.log(vowelWord);
 
 // - Find the index of the word "rhythm"
+console.log(words.indexOf('rhythm'))
+
+// - Create a new array that contains words not containing any vowel.
+function noVowel (string){
+  if(
+    string.toLowerCase().includes  ('a')  ||
+    string.toLowerCase().includes('e')  ||
+    string.toLowerCase().includes('i')   ||
+    string.toLowerCase().includes('o')   || 
+    string.toLowerCase().includes('u')
+   ) {
+    return false
+   } else {
+    return true
+   }
+}
+
+let wordsWithoutVowel = words.filter((w) => noVowel(w))
+console.log(wordsWithoutVowel);
+
+
+// - Create a new array that contains words not starting with vowel.
+
+// Better Approach
+let noVowelAtStart = words.filter((w) => noVowel(w[0]))
+console.log(noVowelAtStart);
+
+
+// - Create a new array that contains words not ending with vowel
+function notEndingWithVowel (string){
+  if(
+    string.toLowerCase().endsWith('a')  ||
+    string.toLowerCase().endsWith('e')  ||
+    string.toLowerCase().endsWith('i')   ||
+    string.toLowerCase().endsWith('o')   || 
+    string.toLowerCase().endsWith('u')
+   ) {
+    return false
+   } else {
+    return true
+   }
+}
+
+let noVowelAtEnd = words.filter((w) => notEndingWithVowel(w))
+console.log(noVowelAtEnd, 'uuu');
+
+// Better Approach 
+let noVowelAtEnd1 = words.filter((w) => noVowel(w[w.length -1]))
+console.log(noVowelAtEnd1, 'yyy');
 
 
 
-// - Create a new array that contians words not starting with vowel.
-
-
-// - Create a new array that contianse words not ending with vowel
-
-
-
-let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+ let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its numbers
+function sumArray (arr){
+return arr.reduce((acc, cv) =>{
+  acc += cv;
+return acc
+}, 0)
+}
 
+sumArray(numbers);
 
+// Another Method
+let sum = numbers.reduce((acc, cv) => {
+return acc += cv;
+}, 0)
+
+console.log(sum);
 // - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...]
-
-
+let multiplyBy3 = numbers.filter((num) => {
+  return num % 3 === 0;
+})
+console.log(multiplyBy3);
 // - Create a new array that contains only even numbers
-
+let even = numbers.filter((num) => num % 2 === 0)
+console.log(even);
 
 // - Create  a new array that contains only odd numbers.
-
+let odd = numbers.filter((num) => num % 2 !== 0)
+console.log(odd);
 
 // - Create a new array that should have true for even number and false for odd numbers.
 
