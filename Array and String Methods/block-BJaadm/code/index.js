64fb3d050aee846214aac2a2f6eb086e4d0e6a48
Aditi Dubey <aditidubey576@gmail.com>
2023-04-07 12:52:06 +0530

    completed few quesions

--- Array and String Methods/block-BJaadm/code/index.js
+++ Array and String Methods/block-BJaadm/code/index.js
@@ -14,40 +14,80 @@ let persons = [
 ];
 
 // Create an array peopleName and store value of name key from persons array
-
-
-
+let peopleName = persons.map((person)=> person.name)
+console.log(peopleName);
 // Create an array peopleGrade and store the value of grade key from persons array
 
-
+let peopleGrade = persons.map((person)=> person.grade)
+console.log(peopleGrade);
 
 // Create an array peopleSex and store the value of sex key from persons array
-
+let peopleSex = persons.map((person)=> person.sex)
+console.log(peopleSex);
 
 // Log the filtered named of people in peopleName that starts with 'J' or 'P'
+let filtered = peopleName.filter((name) => {
+ return  name[0] === 'J' ||
+  name[0] === 'P' 
+})
+console.log(filtered);
+
+// Another Approach 
+let filterd = peopleName.filter((name) => name.startsWith('J') ||  name.startsWith('P') )
+console.log(filterd);
+
 
 
 // Log the length of filtered named of people in peopleName that starts with 'A' and 'C'
-
+let filterdLength = peopleName.filter((name) => name.startsWith('A') ||  name.startsWith('C') )
+console.log(filterdLength.length);
 
 
 // Log all the filtered male ('M') in persons array
-
+let male = persons.filter((person) => person.sex === "M") 
+console.log(male);
 
 // Log all the filtered female ('F') in persons array
-
-
+let female = persons.filter((person) => person.sex === "F") 
+console.log(female);
 
 // Log all the filtered female ('F') whose name starts with 'C' or 'J' in persons array
+let filteredFemale = persons
+.filter((person) => person.sex === "F")
+.filter((person)=>
+ person.name.startsWith("C")  
+  || person.name.startsWith("J") ) ;
+console.log(filteredFemale);
 
+// Another Approach
+/*let filteredFemale = persons
+.filter((person) => person.sex === "F")
+.filter
+((person)=> person.name[0] === "C"  || 
+ person.name[0] === "J");
+console.log(filteredFemale);*/
 
+// Another Approach
+/*let filteredFemale = female.filter((female) => {
+  return female.name.startsWith("C") ||
+  female.name.startsWith("J")
+})
+console.log(filteredFemale);*/
 
 // Log all the even numbers from peopleGrade array
-
-
+let even = peopleGrade.filter((grade) => grade % 2 === 0)
+console.log(even)
 
 // Find the first name that starts with 'J' in persons array and log the object
+let jName = persons.filter((person) => 
+ person.name.startsWith("J"))
+console.log(jName)
 
+// Another Approach
+/*let jName = persons.filter((person) => 
+ person.name[0] === "J")
+
+console.log(jName)*/
 
 // Find the first name that starts with 'P' in persons array and log the object
 
