f954ecdef02d8dbe1e71d9840df22877c6f9f34b
Aditi Dubey <aditidubey576@gmail.com>
2023-05-05 19:58:52 +0530

    iniital commit

---
+++ Introduction to DOM/block-BJaaeb/code/script.js
@@ -0,0 +1,54 @@
+/*
+
+Write a function named `createInputElm` that accepts two parameter (label and `type`) type
+default value to be "text" and return the input element inside label. (create it using `createElement`)
+
+*/
+
+
+
+// TEST
+createInputElm('Your name'); //<label>Your name: <input type="text"></label>
+createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
+
+// 2. Do the same thing as above using string literal like `<h1>Hello</h1>`
+
+
+// Your code goes here
+
+// TEST
+createInputElm('Your name'); //<label>Your name: <input type="text"></label>
+createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
+
+// 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
+// the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
+
+
+
+// TEST
+createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
+createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
+
+// 4. Create a function named `createTodoList` that accept and array of data like [{name: "Learn DOM", isDone: false}, {name: "Learn JS", isDone: true}] and returns
+// the html for single todo will look like given below
+/*
+<ul>
+  <li>
+    <p>Learn DOM</p>
+    <input type="checkbox" checked name="" id="">
+    <span>X</span>
+  </li>
+</ul>
+*/
+
+
+// TEST
+createTodoList([
+  { name: 'Learn DOM', isDone: false },
+  { name: 'Learn JS', isDone: true },
+]);
+createTodoList([
+  { name: 'Learn DOM', isDone: false },
+  { name: 'Learn React', isDone: true },
+  { name: 'Learn JS', isDone: true },
+]);
